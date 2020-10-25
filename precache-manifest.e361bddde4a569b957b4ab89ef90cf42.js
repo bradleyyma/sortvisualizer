6f7e209c7237f3327700b41c0cbc1ecd0ea9a4f1
Bradley Ma <bradley.y.ma@gmail.com>
2020-10-25 14:54:34 -0600

    Updates

---
+++ precache-manifest.e361bddde4a569b957b4ab89ef90cf42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577f5b16f1cdebe1246d5042799dd619",
+    "url": "/sortvisualizer/index.html"
+  },
+  {
+    "revision": "683d6525dacbe5ec4f09",
+    "url": "/sortvisualizer/static/css/main.242e2022.chunk.css"
+  },
+  {
+    "revision": "40b5363757a1a225ad2e",
+    "url": "/sortvisualizer/static/js/2.b2c3f3db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sortvisualizer/static/js/2.b2c3f3db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "683d6525dacbe5ec4f09",
+    "url": "/sortvisualizer/static/js/main.01435f92.chunk.js"
+  },
+  {
+    "revision": "c6a8562fabcd6cd7c741",
+    "url": "/sortvisualizer/static/js/runtime-main.528071fd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sortvisualizer/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sortVisualizer/precache-manifest.fa98a7eba272b2fd002579ca6beebcdd.js"
+  "/sortvisualizer/precache-manifest.e361bddde4a569b957b4ab89ef90cf42.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sortVisualizer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sortvisualizer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
